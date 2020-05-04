462f1e1ad5fcf78e8525d5f0f1af978506508dc8
Yasushi Kumano <yasushi_kumano194@ilc.co.jp>
2020-05-04 17:56:53 +0900

    modify source.

--- jstest/script/own.js
+++ jstest/script/own.js
@@ -18,6 +18,8 @@ class Own extends GControl {
 
         this.oprStartX = 0;
         this.oprStartY = 0;
+        this.tgtPosX = 0;
+        this.tgtPosY = 0;
         this.moveX = 0;
         this.moveY = 0;
         this.tgtRot = 0;
@@ -26,17 +28,26 @@ class Own extends GControl {
     onPress(x, y) {
         this.oprStartX = x;
         this.oprStartY = y;
+        this.tgtPosX = this.posX;
+        this.tgtPosY = this.posY;
         this.spd = 1;
     }
 
     onMove(x, y) {
-        this.moveX = this.oprStartX - x;
-        this.moveY = this.oprStartY - y;
+        this.tgtPosX = this.posX + (x - this.oprStartX) * 1;
+        this.tgtPosY = this.posY + (y - this.oprStartY) * 1;
+        this.moveX = this.posX - this.tgtPosX;
+        this.moveY = this.posY - this.tgtPosY;
+
+        this.spd = Math.sqrt(this.moveX * this.moveX + this.moveY * this.moveY) / 10;
+        if (this.spd > 2) {
+            this.spd = 2;
+        }
     }
 
     onRelease(x, y) {
-//        this.moveX = 0;
-//        this.moveY = 0;
+        //        this.moveX = 0;
+        //        this.moveY = 0;
         this.spd = 0;
     }
 
