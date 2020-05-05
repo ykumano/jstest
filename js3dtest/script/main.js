f33960d5978a53e0db933a6785994cf97679cd99
Yasushi Kumano <yasushi_kumano194@ilc.co.jp>
2020-05-05 14:17:56 +0900

    modify source.

--- js3dtest/script/main.js
+++ js3dtest/script/main.js
@@ -1,31 +1,86 @@
-﻿var scene = new THREE.Scene();
+﻿window.onload = function() {
+  var updateListeners = [];
+  var update = function(frame) {
+      updateListeners.forEach(function(listener) {
+          listener(frame);
+      });
+  };
+  
+  var scene = new THREE.Scene();
 
-// パーサーを作ります
-var parser = new vox.Parser();
+  var camera = new THREE.PerspectiveCamera(70, 1, 0.1, 1000);
+  var cameraTarget = new THREE.Vector3(0, 10, 0);
+  updateListeners.push(function(frame) {
+      camera.position.x = Math.cos(frame * 0.004) * 50;
+      camera.position.y = 15;
+      camera.position.z = Math.sin(frame * 0.004) * 50;
+      camera.lookAt(cameraTarget);
+  });
+  
+  var directionalLight = new THREE.DirectionalLight(0xffffff);
+  updateListeners.push(function(frame) {
+      directionalLight.position.x = Math.cos(frame * -0.001) * 100;
+      directionalLight.position.y = 100;
+      directionalLight.position.z = Math.sin(frame * -0.001) * 100;
+  });
+  scene.add(directionalLight);
 
-// *.voxファイルを読み込みます
-parser.parse("res/SolValou.vox").then(function(voxelData) { // ←ボクセルデータが取れます
+  var ambientLight = new THREE.AmbientLight(0xaaaaaa);
+  scene.add(ambientLight);
+  
+  // var ground = new THREE.Mesh(new THREE.PlaneGeometry(300, 300), new THREE.MeshPhongMaterial({
+  //     color: 0x555555,
+  //     shininess: 1,
+  // }));
+  // ground.rotation.x = Math.PI * -0.5;
+  // scene.add(ground);
+  
+  var renderer = new THREE.WebGLRenderer({
+      canvas: document.getElementById("c")
+  });
+  renderer.setSize(1024, 1024);
+  renderer.setClearColor(0x000000);
 
-  // データ全体の大きさ
-  voxelData.size; // => { x: number, y: number, z: number }
+  var parser = new vox.Parser();
+  var parseTasks = [
+      // "vox/mv0.98/p20.vox",
+      "res/SolValou.vox",
+  ].map(function(path) {
+      return parser.parse(path);
+  });
+  Promise
+      .all(parseTasks)
+      .then(function(voxelDataArray) {
+          voxelDataArray.forEach(function(voxelData, i) {
+              var builder = new vox.MeshBuilder(voxelData, {
+                  voxelSize: 1.0,
+                  vertexColor: false,
+                  optimizeFaces: true,
+              });
+              var mesh = builder.createMesh();
+              
+              // mesh.position.x = Math.random() * 100 - 50;
+              // mesh.position.z = Math.random() * 100 - 50;
 
-  // ボクセルの配列
-  voxelData.voxels; // => [Voxel, Voxel, Voxel, ...]
-
-  // ボクセル一個のデータ
-  voxelData.voxels[0]; // => { x: number, y: number, z: number, colorIndex: number }
-
-  // カラーパレット
-  voxelData.palette; // => [Color, Color, Color, ...]
-  voxelData.palette[0]; // => { r: number, g: number, b: number, a: number }
-
-// ビルダーを作ります。引数にボクセルデータをわたします
-var builder = new vox.MeshBuilder(voxelData);
-// THREE.Meshを作ります
-var mesh = builder.createMesh();
-
-// THREE.Sceneに追加するなどして使ってください
-scene.add(mesh);
-
-});
+              // mesh.rotation.y = Math.random() * Math.PI * 2;
+              
+              var fv = new THREE.Vector3();
+              // updateListeners.push(function(frame) {
+              //     fv.set(0, 0, 0.1);
+              //     fv.applyQuaternion(mesh.quaternion);
+              //     mesh.position.add(fv);
+              // });
+              
+              scene.add(mesh);
+          });
+      });
+  
+  var frame = 0;
+  var render = function() {
+      update(frame++);
+      renderer.render(scene, camera);
+      requestAnimationFrame(render);
+  };
+  render();
+};
 
