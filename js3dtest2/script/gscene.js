0b6a578ecd8bc9c0e8aba268967f5fd292d48052
Yasushi Kumano <yasushi_kumano194@ilc.co.jp>
2020-05-08 20:19:50 +0900

    modify source.

--- js3dtest2/script/gscene.js
+++ js3dtest2/script/gscene.js
@@ -12,9 +12,63 @@ class GScene {
          * @type {HTMLCanvasElement}
          */
         this.canvasElement = canvas;
-
+        console.log("canvas:" + canvas);
         this.scene = new THREE.Scene();
 
-        return this.scene;
+        this.renderer = new THREE.WebGLRenderer({
+            canvas: this.canvasElement
+        });
+        this.renderer.setSize(1024, 1024);
+        this.renderer.setClearColor(0x000000);
+
+        this.updateListeners = [];
+
+        //        return this.scene;
     }
+
+    createCamera(updateCamera) {
+        /** カメラの生成 */
+        this.camera = new THREE.PerspectiveCamera(70, 1, 0.1, 1000);
+        this.cameraTarget = new THREE.Vector3(0, 30, 0);
+        this.camera.position.x = -5;
+        this.camera.position.y = 10;
+        this.camera.position.z = 50;
+
+        var ccamera = this.camera;
+        var ccamerat = this.cameraTarget;
+
+        this.updateListeners.push(function (frame) {
+            updateCamera(ccamera, ccamerat, frame);
+        });
+    }
+
+    createLight(updateLight) {
+        /**　光源の生成 */
+        this.directionalLight = new THREE.DirectionalLight(0xffffff);
+
+        var light = this.directionalLight;
+
+        this.updateListeners.push(function (frame) {
+            updateLight(light, frame);
+        });
+        this.scene.add(this.directionalLight);
+
+        this.ambientLight = new THREE.AmbientLight(0xaaaaaa);
+        this.scene.add(this.ambientLight);
+
+    }
+
+    addMesh(mesh) {
+        this.scene.add(mesh);
+    }
+
+    renderMesh() {
+        this.renderer.render(this.scene, this.camera);
+    }
+
+    update(frame) {
+        this.updateListeners.forEach(function (listener) {
+            listener(frame);
+        });
+    };
 }
--- js3dtest2/script/main.js
+++ js3dtest2/script/main.js
@@ -1,20 +1,9 @@
 ﻿window.onload = function () {
-    var updateListeners = [];
     var meshList = [];
     var readNum = 0;
 
-    /**
-     * 描画更新処理
-     * @param {*} frame 
-     */
-    var update = function (frame) {
-        updateListeners.forEach(function (listener) {
-            listener(frame);
-        });
-    };
-
-    var scene = new GScene();
-
+    var gscene = new GScene(document.getElementById("c"));
+    /*
     updateListeners.push(function (frame) {
         if(readNum == 2){
             for(i=0; i<2; i++){
@@ -27,51 +16,26 @@
             }
         }
     });
-
-    /** カメラの生成 */
-    var camera = new THREE.PerspectiveCamera(70, 1, 0.1, 1000);
-    var cameraTarget = new THREE.Vector3(0, 30, 0);
-    camera.position.x = -5;
-    camera.position.y = 10;
-    camera.position.z = 50;
-    updateListeners.push(function (frame) {
-        /**
-         * カメラ位置更新処理
-         */
-/*
-         camera.position.x = Math.cos(-frame * 0.04) * 50;
-        camera.position.y = 15;
-        camera.position.z = Math.sin(-frame * 0.04) * 50;
-        camera.lookAt(cameraTarget);
 */
-    });
+    function updateCamera(camera, cameraTarget, frame) {
+        /*
+                camera.position.x = Math.cos(-frame * 0.04) * 50;
+                camera.position.y = 15;
+                camera.position.z = Math.sin(-frame * 0.04) * 50;
+                camera.lookAt(cameraTarget);
+        */
+    }
+    gscene.createCamera(updateCamera);
 
-    /**　光源の生成 */
-    var directionalLight = new THREE.DirectionalLight(0xffffff);
-    updateListeners.push(function (frame) {
-        /** 光源位置更新処理 */
+    function updateLight(directionalLight, frame) {
         directionalLight.position.x = Math.cos(frame * -0.001) * 100;
         directionalLight.position.y = 100;
         directionalLight.position.z = Math.sin(frame * -0.001) * 100;
-    });
-    scene.add(directionalLight);
+    }
+    gscene.createLight(updateLight);
 
-    var ambientLight = new THREE.AmbientLight(0xaaaaaa);
-    scene.add(ambientLight);
-
-    // var ground = new THREE.Mesh(new THREE.PlaneGeometry(300, 300), new THREE.MeshPhongMaterial({
-    //     color: 0x555555,
-    //     shininess: 1,
-    // }));
-    // ground.rotation.x = Math.PI * -0.5;
-    // scene.add(ground);
-
-    var renderer = new THREE.WebGLRenderer({
-        canvas: document.getElementById("c")
-    });
-    renderer.setSize(1024, 1024);
-    renderer.setClearColor(0x000000);
-
+    var solvalou = null;
+    var rynex = null;
     var parser = new vox.Parser();
     var parseTasks = [
         "res/SolValou.vox",
@@ -91,29 +55,34 @@
                 });
                 var mesh = builder.createMesh();
 
-                // solMesh.position.x = Math.random() * 100 - 50;
-                // solMesh.position.z = Math.random() * 100 - 50;
-
-                // solMesh.rotation.y = Math.random() * Math.PI * 2;
-
-                var fv = new THREE.Vector3();
-                // updateListeners.push(function(frame) {
-                //     fv.set(0, 0, 0.1);
-                //     fv.applyQuaternion(mesh.quaternion);
-                //     mesh.position.add(fv);
-                // });
-
-//                solMesh.rotation.z = Math.PI / 3;
-                meshList.push(mesh);
-                scene.add(mesh);
+                meshList[i] = mesh;
                 readNum++;
             });
+        })
+        .then(function () {
+            console.log("read:"+readNum);
+            solvalou = meshList[0];
+            rynex = meshList[1];
+
+            gscene.addMesh(solvalou);
+            gscene.addMesh(rynex);
         });
+
+
+
+    function updateFrame() {
+        solvalou.rotation.y += 0.03;
+        rynex.rotation.x += 0.03;
+    }
 
     var frame = 0;
     var render = function () {
-        update(frame++);
-        renderer.render(scene, camera);
+        gscene.update(frame++);
+        if(readNum == 2){
+            updateFrame();
+        }
+
+        gscene.renderMesh();
         requestAnimationFrame(render);
     };
     render();
